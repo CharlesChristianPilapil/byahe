a1290ffad2bfbc3c05d0040e9f43ecf478bef51c
CharlesChristianPilapil <charleschristian.pilapil@gmail.com>
2024-02-21 10:19:14 +0800

    scroll added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import { Route, Routes } from "react-router-dom"
-import Header from "./components/Header"
-import Home from "./pages/home/Home"
-import About from "./pages/about/About"
-import Services from "./pages/service/Services"
-import Contact from "./pages/contact/Contact"
-import Gallery from "./pages/gallery/Gallery"
-import Footer from "./components/Footer"
-import Section from "./components/Section"
-import Newsletter from "./components/Newsletter"
+import { useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Services from "./pages/service/Services";
+import Contact from "./pages/contact/Contact";
+import Gallery from "./pages/gallery/Gallery";
+import Footer from "./components/Footer";
+import Section from "./components/Section";
+import Newsletter from "./components/Newsletter";
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const handleScrollToTop = () => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+
+    // Scroll to top whenever the location changes
+    handleScrollToTop();
+  }, [location]);
 
   return (
     <>
@@ -22,13 +33,13 @@ function App() {
           <Route path="gallery" element={<Gallery />} />
           <Route path="contact" element={<Contact />} />
         </Routes>
-        <Section title='Newsletter'>
+        <Section title="Newsletter">
           <Newsletter />
         </Section>
       </main>
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
